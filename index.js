3df653f3a347a964386b765f402213612684f7af
Vipul Dessai <badgamerbad@gmail.com>
2020-12-02 19:26:45 +0530

    updated serving file

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/bulid'));
+// Serve only the static files form the bulid directory
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/build/index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(path.join(__dirname, 'build', 'index.html')));
 });
 
 // Start the app by listening on the default Heroku port
