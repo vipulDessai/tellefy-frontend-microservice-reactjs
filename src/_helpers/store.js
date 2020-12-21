ed221f9784df1c8cebf1b73f566b0d70b80b4fb5
Vipul Dessai <badgamerbad@gmail.com>
2020-12-22 01:17:02 +0530

    redux store debug

--- src/_helpers/store.js
+++ src/_helpers/store.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../_reducers';
 
 const loggerMiddleware = createLogger();
+const applyMidlewareRef = applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+);
 
 export const store = createStore(
     rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+    NODE_ENV == 'development' ? composeWithDevTools(applyMidlewareRef) : applyMidlewareRef,
 );
