4b398258151e62b7a587d48cb184dd15c015f50e
Vipul Dessai <badgamerbad@gmail.com>
2020-12-25 01:24:49 +0530

    generated dynamic vars in js

--- jest-configs/setup-tests.js
+++ jest-configs/setup-tests.js
@@ -1,5 +1,15 @@
+// Read all the environment variables
+import { config } from 'dotenv';
+config();
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// since the window is global and its keys can be accessed
+// it will serve the ENV attributes
+for(let envKey in process.env) {
+    window[envKey] = JSON.stringify(process.env[envKey]);
+}
