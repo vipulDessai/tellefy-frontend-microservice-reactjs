dc3a77f141aceaac43c58040c34bec5b4d32f4cd
Vipul Dessai <badgamerbad@gmail.com>
2021-01-03 15:37:06 +0530

    added ts support for jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/jest-configs/setup-tests.js'],
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    globals: {
+        'ts-jest': {
+            'babelConfig': true,
+            'tsconfig': 'tsconfig.json'
+        }
+    },
+    setupFilesAfterEnv: [
+        '<rootDir>/jest-configs/setup-tests.js',
+    ],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/jest-configs/no-parser-stub.js',
-        '\\.(css|scss)$': '<rootDir>/jest-configs/no-parser-stub.js'
+        '\\.(css|scss)$': '<rootDir>/jest-configs/no-parser-stub.js',
     },
+    // Module file extensions for importing
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { App } from './App';
+import { App } from '@/App';
 
 import { store } from '@/_helpers';
 
